19b49783192b2ca84676792746e64dce518e7211
klapinsk <katarzyna.lapinska@intel.com>
2019-04-04 11:19:10 +0200

    Tests for flush method

--- tests/functional/tests/conftest.py
+++ tests/functional/tests/conftest.py
@@ -3,18 +3,19 @@
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 #
 
-import os
+
 import sys
+import os
 import pytest
 
 sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
+
 from pyocf.types.logger import LogLevel, DefaultLogger, BufferLogger
 from pyocf.types.volume import Volume, ErrorDevice
 from pyocf.types.ctx import get_default_ctx
-
-
-def pytest_configure(config):
-    sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
+from pyocf.utils import Size
+from pyocf.types.cache import Cache
+from pyocf.types.core import Core
 
 
 @pytest.fixture()
@@ -37,3 +38,18 @@ def pyocf_ctx_log_buffer():
     yield logger
     for cache in c.caches:
         cache.stop()
+
+
+@pytest.fixture()
+def start_cache_with_core():
+    def start_with_config(cache_size: Size, core_size: Size, **config):
+        cache_device = Volume(cache_size)
+        core_device = Volume(core_size)
+        cache = Cache.start_on_device(cache_device, **config)
+        core = Core.using_device(core_device)
+        cache.add_core(core)
+        cache_device.reset_stats()
+        core_device.reset_stats()
+        return cache_device, core_device, cache, core
+
+    return start_with_config
