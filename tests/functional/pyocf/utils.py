f36902fae2331c9962c0afff8fa12c75e4826539
Kamil Lepek <kamil.lepek94@gmail.com>
2019-04-16 13:44:47 +0200

    Add security fuzzy tests and improve pyocf api
    Fuzzy tests added for:
    changing cache mode
    attaching cache device with fuzzed cache line size
    setting cleaning policy
    setting alru params
    setting acp params
    setting seq cut off policy per cache
    setting seq cut off policy per core

--- tests/functional/pyocf/utils.py
+++ tests/functional/pyocf/utils.py
@@ -3,7 +3,16 @@
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 #
 
-from ctypes import string_at
+import random
+import string
+from ctypes import (
+    c_uint64,
+    c_uint32,
+    c_uint16,
+    c_int,
+    c_uint,
+    string_at
+)
 
 
 def print_buffer(buf, length, offset=0, width=16, stop_after_zeros=0):
@@ -15,7 +24,6 @@ def print_buffer(buf, length, offset=0, width=16, stop_after_zeros=0):
 
     for addr in range(offset, end, width):
         cur_line = buf[addr : min(end, addr + width)]
-        all_zeros = True
         byteline = ""
         asciiline = ""
         if not any(cur_line):
@@ -138,3 +146,41 @@ def struct_to_dict(struct):
         d[field] = value
 
     return d
+
+
+def generate_random_numbers(c_type):
+    type_dict = {
+        c_uint16: [0, c_uint16(-1).value],
+        c_uint32: [0, c_uint32(-1).value],
+        c_uint64: [0, c_uint64(-1).value],
+        c_int: [int(-c_uint(-1).value / 2) - 1, int(c_uint(-1).value / 2)]
+    }
+
+    values = []
+    for i in range(0, 1000):
+        values.append(random.randint(type_dict[c_type][0], type_dict[c_type][1]))
+    return values
+
+
+def generate_random_strings():
+    values = []
+    for t in [string.digits,
+              string.ascii_letters + string.digits,
+              string.ascii_lowercase,
+              string.ascii_uppercase,
+              string.printable,
+              string.punctuation,
+              string.hexdigits]:
+        for i in range(0, 100):
+            values.append(''.join(random.choice(t) for _ in range(random.randint(0, 20))))
+    return values
+
+
+def get_random_ints(c_type):
+    for value in generate_random_numbers(c_type):
+        yield value
+
+
+def get_random_strings():
+    for value in generate_random_strings():
+        yield value
